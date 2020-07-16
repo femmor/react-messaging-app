491beb758272b450eb812e19623115b3741f6788
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-16 14:06:13 -0500

    add comments to code

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -19,37 +19,51 @@ class Register extends Component {
     }
 
     isFormValid = () => {
+        // errors array // also add initial value to state
         let errors = []
+        // error variable
         let error
 
         // check if form is empty or not
+        // Pass state object to the isFormEmpty function
         if(this.isFormEmpty(this.state)){
-            // throw error
+            // throw an error if form is empty
+            // assign error variable to an object to define the message error
             error = { message: "Please fill in all fields" }
+            // set the state and concat error to the errors array
             this.setState({ 
                 errors: errors.concat(error)
              })
 
             return false
+            // Pass state object to the passwordValid function
+            // only password and passwordConfirm parameters should be used
         } else if(!this.isPasswordValid(this.state)) {
-            // throw error
+            // throw an error if password is invalid
             error = {message: "Password is invalid"}
+            // set the state and concat error to the errors array
             this.setState({
                 errors: errors.concat(error)
             })
 
             return false
         } else {
-            // form is valid
+            // Return true if both conditions are met
             return true
         }
     }
 
+    // Is form empty? function
     isFormEmpty = ({ email, username, password, passwordConfirm }) => {
+        // if form input length === 0, return true
+        // indicating that form is not entirely filled out
+        // throw an error if form is empty
         return !email.length || !username.length || !password.length || !passwordConfirm.length
     }
 
+    // is password valid? function
     isPasswordValid = ({ password, passwordConfirm }) => {
+        // Check the length of both values and return false if they are less than 6
         if (password.length < 6 || passwordConfirm.length < 6) {
             return false;
         } else if (password !== passwordConfirm) {
@@ -59,23 +73,31 @@ class Register extends Component {
         }
       };
 
+    // Display errors
+    // take the errors array in the state
+    // map over the errors to get individual errors
     displayError = errors => errors.map((error, i) => {
         return (
+            // return a set of p tags with the error message
             <p key={i}>{error.message}</p>
         )
     })
 
+    // Handle form change
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
+    // Handle form submit
     handleSubmit = (e) => {
         e.preventDefault()
 
         // check if form is valid
         if (this.isFormValid()) {
+            // before reaching out to firebase, 
+            // set the state and clear the errors array and set loading to true
             this.setState({
                 errors: [],
                 loading: true
@@ -87,15 +109,18 @@ class Register extends Component {
                 .auth()
                 .createUserWithEmailAndPassword(email, password)
                 .then(createdUser => {
-                    console.log(createdUser)
-
+                    // update createdUser data with display name and avatar from gravatar
                     createdUser.user.updateProfile({
                         displayName: username,
                         photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon` 
                     })
                     .then(() => {
                         this.saveUser(createdUser).then(() => {
-                            console.log("user saved")
+                            console.log("user saved!")
+                            // Set loading property to false in the state
+                            this.setState({
+                                loading: false
+                            })
                         })
                     })
                     .catch(err => {
@@ -117,6 +142,7 @@ class Register extends Component {
         }
     }
 
+    // Save user to firebase Realtime database 
     saveUser = createdUser => {
         return this.state.usersRef.child(createdUser.user.uid).set({
             name: createdUser.user.displayName,
@@ -124,7 +150,9 @@ class Register extends Component {
         })
     }
 
+    // Handle input errors
     handleInputErrors = (errors, inputName) => {
+        // convert the error message to lowercase and check if it includes the input name return the error class or an empty string
         return errors.some(error => error.message.toLowerCase().includes(inputName))
           ? "error"
           : "";
@@ -132,7 +160,7 @@ class Register extends Component {
     
 
     render() {
-
+        // Destructure the state
         const { username, email, password, passwordConfirm, errors, loading } = this.state
 
         return (
@@ -151,9 +179,11 @@ class Register extends Component {
                             <Button disabled={loading} className={loading ? "loading" : ""} color="violet" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
-
+                    {/* display any errors */}
+                    {/* check if there are errors in the errors array */}
                     {
                         errors.length > 0 && (
+                            // show message component to display error
                             <Message error>
                                 <h4>Error</h4>
                                 {this.displayError(errors)}
