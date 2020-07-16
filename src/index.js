3877afdd7d3cc52f83a46d7f53cf2799f15d635a
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-16 15:51:15 -0500

    Set up Redux to Make User Data Available on Global State

--- src/index.js
+++ src/index.js
@@ -1,45 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import firebase from "./firebase"
 import App from './components/App';
-
-import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom"
 
 // Components
 import Login from "./components/Auth/Login"
 import Register from "./components/Auth/Register"
 
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom"
+import { createStore } from "redux"
+import { Provider, connect } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import rootReducer from './reducers';
+import { setUser } from "./actions"
+
+const store = createStore(rootReducer, composeWithDevTools())
+
 class Root extends Component {
 
-  componentDidMount() {
-    // Run cdm to know whether there's a user in the app
-    firebase
-      .auth()
-      .onAuthStateChanged(user => {
-        // Check if there's a user then 
-        if (user) {
-          this.props.history.push("/")
-        }
-      })
-  }
-  
+    componentDidMount() {
+        // Run cdm to know whether there's a user in the app
+        firebase
+            .auth()
+            .onAuthStateChanged(user => {
+                // Check if there's a user then 
+                if (user) {
+                    this.props.setUser(user)
+                    this.props.history.push("/")
+                }
+            })
+    }
 
-  render() {
-    return (
-        <Switch>
-          <Route path="/" exact component={App}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/register" component={Register}/>
-        </Switch>
-    )
-  }
+
+    render() {
+        return ( 
+            <Switch>
+                <Route path="/"exact component={App}/>
+                <Route path="/login" component={Login}/> 
+                <Route path="/register" component={Register}/> 
+            </Switch>
+        )
+    }
 }
 
-const RootWithAuth = withRouter(Root)
+const RootWithAuth = withRouter(connect(null, {setUser})(Root))
 
-ReactDOM.render(
-    <Router>
-      <RootWithAuth />
-    </Router>,
-  document.getElementById('root')
+ReactDOM.render( 
+    <Provider store = { store } >
+        <Router >
+            <RootWithAuth />
+        </Router> 
+    </Provider > ,
+    document.getElementById('root')
 );
