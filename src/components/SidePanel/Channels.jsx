c24cf313185fb66d691070cfe98eaac700433fc7
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-17 16:02:15 -0500

    Create Channels Collection, Add First Channel to Database

--- src/components/SidePanel/Channels.jsx
+++ src/components/SidePanel/Channels.jsx
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
+import firebase from "../../firebase"
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react"
 
 class Channels extends Component {
 
     state = {
+        user: this.props.currentUser,
         channels: [],
         channel_name: "",
         channel_details: "",
+        channelsRef: firebase.database().ref("channels"),
         modal: false
     }
 
+    // Handle submit method
+    handleSubmit = e => {
+        e.preventDefault()
+        if (this.isFormValid(this.state)) {
+            this.addChannel()
+        } 
+    }
+
+    // Check if the form has valid inputs
+    isFormValid = ({ channel_name, channel_details }) => channel_name && channel_details
+
+    // addChannel method
+    addChannel = () => {
+        // Destructure the channelsRef from the state 
+        const { channelsRef, channel_name, channel_details, user } = this.state
+
+        // Set channel key
+        const key = channelsRef.push().key
+
+        // Create new channel
+        const newChannel = {
+            id: key,
+            name: channel_name,
+            details: channel_details,
+            createdBy: {
+                name: user.displayName,
+                avatar: user.photoURL
+            }
+        }
+
+        // Take channels ref and 
+        channelsRef
+            .child(key)
+            .update(newChannel)
+            .then(() => {
+                this.setState({ channel_name: "", channel_details: "" })
+                this.closeModal()
+                console.log("channel added!")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        
+    }
+
+    // open modal method
     openModal = () => {
         this.setState({
             modal: true
         })
     }
 
+    // close modal method
     closeModal = () => {
         this.setState({
             modal: false
         })
     }
 
+    // handleChange method
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
@@ -56,7 +107,7 @@ class Channels extends Component {
                         Add a Channel
                     </Modal.Header>
                     <Modal.Content>
-                        <Form onSubit={this.handleSubmit}>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Field>
                                 <Input 
                                     fluid
@@ -78,7 +129,7 @@ class Channels extends Component {
                         </Form>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button color="green" inverted>
+                        <Button color="green" inverted onClick={this.handleSubmit}>
                             <Icon name="checkmark"/> Add Channel
                         </Button>
                         <Button color="red" inverted onClick={this.closeModal}>
--- src/components/SidePanel/index.jsx
+++ src/components/SidePanel/index.jsx
@@ -19,7 +19,7 @@ class SidePanel extends Component {
                 }}
             >
                 <UserPanel currentUser={currentUser}/>
-                <Channels />
+                <Channels currentUser={currentUser}/>
             </Menu>
         );
     }
