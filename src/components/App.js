480842e3bb6d6246a7cc6beb1a03e0d598b96d56
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-17 12:31:13 -0500

    Scaffold App Basic Structure and Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Grid } from "semantic-ui-react"
 import "../components/App.css"
+import ColorPanel from './ColorPanel';
+import SidePanel from './SidePanel';
+import Messages from './Messages';
+import MetaPanel from './MetaPanel';
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Grid columns="equal" className="app" style={{
+            background: "#eee"
+        }}>
+            <ColorPanel/>
+            <SidePanel/>
+            
+            <Grid.Column style={{
+                marginLeft: 320
+            }}>
+                <Messages/>
+            </Grid.Column>
+
+            <Grid.Column width={4}>
+                <MetaPanel/>
+            </Grid.Column>
+            
+        </Grid>
     );
 }
 
