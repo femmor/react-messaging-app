ca234882284de4535f415cf9fe096bf0c7e9962f
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-17 14:33:25 -0500

    Add Channels Component

---
+++ src/components/SidePanel/Channels.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Menu, Icon } from "semantic-ui-react"
+
+class Channels extends Component {
+
+    state = {
+        channels: []
+    }
+
+    render() {
+        const { channels } = this.state
+        return (
+            <Menu.Menu
+                style={{
+                    paddingBottom: "2em"
+                }}
+            >
+                <Menu.Item>
+                    <span>
+                        <Icon name="exchange"/>
+                        CHANNELS
+                    </span> ({ channels.length }) <Icon name="add"/>
+                </Menu.Item>
+                {/* Show all channels in state */}
+            </Menu.Menu>
+        );
+    }
+}
+
+export default Channels;
--- src/components/SidePanel/index.jsx
+++ src/components/SidePanel/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from "./UserPanel"
+import Channels from "./Channels"
 
 class SidePanel extends Component {
     
@@ -18,6 +19,7 @@ class SidePanel extends Component {
                 }}
             >
                 <UserPanel currentUser={currentUser}/>
+                <Channels />
             </Menu>
         );
     }
